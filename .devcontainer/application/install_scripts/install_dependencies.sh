801e56b5e1c17fb0426836608dc006de28367c7a
Thomas Willson <thomas.willson@me.com>
2021-06-13 14:11:15 -0700

    Add debug support.

--- .devcontainer/application/install_scripts/install_dependencies.sh
+++ .devcontainer/application/install_scripts/install_dependencies.sh
@@ -5,6 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -qq update
 apt-get -y -qq install \
   git nginx \
-  php7.4 php7.4-fpm php7.4-mysql php7.4-gd php7.4-curl php7.4-apcu php7.4-cli php7.4-json php7.4-mbstring php7.4-zip \
+  php7.4 php7.4-fpm php7.4-mysql php7.4-gd php7.4-curl php7.4-apcu php7.4-cli php7.4-json php7.4-mbstring php7.4-zip php7.4-xdebug \
   ssh wget vim less zip cron lsof sudo supervisor python3-pygments mysql-client gcc g++
 
